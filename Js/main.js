1f59ba9478a5254460a2fc8e029c975177e1de8e
Darko Sulekic <darkovsulekic@gmail.com>
2022-03-24 19:04:30 -0300

    Ejercicio js

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,17 @@
-let entrada1 = parseInt(prompt("Ingrese un numero")) 
+/* Ejericio 1 */
+
+/* let entrada1 = parseInt(prompt("Ingrese un numero")) 
 let entrada2 = parseInt (prompt("Ingrese otro numero"))
 let resultado = entrada1 + entrada2
-alert(resultado)
+alert(resultado) */
+
+/* Ejercicio 2 */
+
+let nombreIngresado = prompt("Ingresar Nombre");
+
+
+if ((nombreIngresado !="") && ((nombreIngresado == "Ema") || (nombreIngresado== "EMA"))) {
+    alert("Hola ema")
+} else{
+    alert("Invalido")
+}
