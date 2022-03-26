d0b8569e89d05d46f83747320db8af451d57d83e
Darko Sulekic <darkovsulekic@gmail.com>
2022-03-26 16:14:14 -0300

    Primer desafio complementario

--- Js/main.js
+++ Js/main.js
@@ -1,17 +1,37 @@
-/* Ejericio 1 */
-
-/* let entrada1 = parseInt(prompt("Ingrese un numero")) 
-let entrada2 = parseInt (prompt("Ingrese otro numero"))
-let resultado = entrada1 + entrada2
-alert(resultado) */
-
-/* Ejercicio 2 */
-
-let nombreIngresado = prompt("Ingresar Nombre");
 
 
-if ((nombreIngresado !="") && ((nombreIngresado == "Ema") || (nombreIngresado== "EMA"))) {
-    alert("Hola ema")
-} else{
-    alert("Invalido")
+
+/* 
+let precio = parseInt(prompt("Ingresar numero"))
+
+if (isNaN(precio)) {
+    alert("No es un numero burraso")
+} else {
+    if (precio < 30) {
+        alert("El precio es menor que 30")
+    }
+    else if (precio< 50){
+        alert("El precio es menor que 50")
+    }
+    else if (precio < 100){
+        alert("El precio es menor que 100")
+    } 
+    else if (precio== 100) {
+        alert("Señora tiene cambio de 100?")
+    }
+    else if (precio>100){
+        alert("El precio es mayor 100")
+    }
+} */
+
+/* DESAFIO COMPLEMENTARIO */
+
+let randomNumber = Math.floor(Math.random()*3)
+let userNumber = parseInt(prompt("Ingrese un numero del 0 al 3"))
+while (userNumber != randomNumber) {
+    userNumber = parseInt(prompt("No adivinaste, ingrese otro numero"))
+}if (randomNumber == userNumber) {
+    alert("Advinaste el numero")
 }
+
+
