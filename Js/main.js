5d369439d2702294cb48dc7137566f91ca0b0cd2
Darko Sulekic <darkovsulekic@gmail.com>
2022-04-05 11:37:14 -0300

    Desafio complementario

--- Js/main.js
+++ Js/main.js
@@ -1,29 +1,6 @@
 
 
 
-/* 
-let precio = parseInt(prompt("Ingresar numero"))
-
-if (isNaN(precio)) {
-    alert("No es un numero burraso")
-} else {
-    if (precio < 30) {
-        alert("El precio es menor que 30")
-    }
-    else if (precio< 50){
-        alert("El precio es menor que 50")
-    }
-    else if (precio < 100){
-        alert("El precio es menor que 100")
-    } 
-    else if (precio== 100) {
-        alert("Señora tiene cambio de 100?")
-    }
-    else if (precio>100){
-        alert("El precio es mayor 100")
-    }
-} */
-
 /* DESAFIO COMPLEMENTARIO */
 
 let randomNumber = Math.floor(Math.random()*3)
@@ -41,18 +18,32 @@ function solicitarNombre () {
 /* PRIMER DESAFIO ENTREGABLE */
 
 
-
-
-
-
-let dividir = (a,b)=> a/b
+/* let dividir = (a,b)=> a/b
 let userResponse = prompt("Desea calcular las cuotas?")
+class producto {
+    constructor (nombre,precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+        showPrice() {
+            alert ("El precio de este producto es de " + this.precio)
+        }
+        showProduct() {
+            alert("El producto seleccionado es " + this.nombre)
+        }
+}
+const producto1 = new producto ("Televisor",12000);
+
+
 function calcularCuotas (producto,precio,cuotas){
     alert (`El ${producto} se pagara en ${cuotas} cuotas de ${dividir(precio,cuotas)}`)
 }
 while (userResponse =="Si" || userResponse=="si") {
-let producto = prompt("Ingresar un producto")
-let precio = parseInt(prompt("Ingresar precio"))
+
+let producto = producto1.nombre
+let precio = producto1.precio
+producto1.showProduct()
+producto1.showPrice()
 let cuotas = parseInt(prompt("Ingresar cuotas"))
 
  calcularCuotas(producto,precio,cuotas)
@@ -61,10 +52,67 @@ let cuotas = parseInt(prompt("Ingresar cuotas"))
 if (userResponse != "Si") {
     alert("Gracias por su compra")
 } 
+ */
 
 
+/* const producto2 = new producto ("Camara",1200);
+const producto3 = new producto ("Cama",12000);
+const producto4 = new producto ("Celular",12000);
+const producto5 = new producto ("Pantalla",12000);
+const producto6 = new producto ("Mouse",12000);
+
+console.log (producto1)
+console.log (producto2)
+console.log (producto3) */
+/* OBJETOS */
+
+/* let nombre = "Darko";
+let edad = 26;
+let calle = "Leguizamon 979"
+
+const persona1 = { nombre:"Darko", edad:26,calle:"Leguizamon 979" }
+
+console.log(persona1.nombre)
+console.log(persona1.edad)
+console.log(persona1.calle)
 
 
+let cadena = ("Hola Darko")
+
+console.log (cadena.length)
+console.log (cadena.toLowerCase())
+console.log (cadena.toUpperCase()
+) */
+
+/* function Persona (nombre,edad,calle) {
+    this.nombre = nombre;
+    this.edad= edad;
+    this.calle= calle;
+    this.hablar = function () {console.log ("Hola soy "+ this.nombre)}
+}
+
+const persona1 = new Persona("Darko", 26, "Leguizamon979")
+const persona2 = new Persona("Agustin", 31, "Necochea147")
+
+persona1.hablar(); */
+/* 
+const persona1 = { nombre: "Homero", edad: 39, calle: "Av. Siempreviva 742"}
+
+for (const propiedad in persona1) {
+    console.log(persona1);
+}
+
+for (const propiedad in persona1) {
+    console.log(persona1 [propiedad]);
+} */
 
 
+/* var countSheep = function (num){
+    let sheep = "";
 
+for (let i = 1 ; i<=num; i++){
+  sheep+= i + ` sheep...`
+}
+return sheep;
+}
+countSheep(1) */
